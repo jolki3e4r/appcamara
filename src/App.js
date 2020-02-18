5a44c63f12bdaa75fb5353687e4eff6f8cd13c2b
jolki3e4r <jolki3e4r@hotmail.com>
2020-02-17 23:26:11 -0500

    creando el formulario

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Login from '../src/components/Login.js'
+
+class App extends Component {
+   
+    render() { 
+        return ( 
+            <Login />
+         );
+    }
+}
+ 
+export default App;
